64498d149e0eb9a64bc1cd05c9d2458fc2014c6a
ajm49 <albert.murph@gmail.com>
2022-07-16 01:41:03 -0400

    no change to two endpoints

--- auth.js
+++ auth.js
@@ -2,6 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = async (request, response, next) => {
   try {
+
+
     //   get the token from the authorization header
     const token = await request.headers.authorization.split(" ")[1];
 
@@ -16,7 +18,7 @@ module.exports = async (request, response, next) => {
 
     // pass down functionality to the endpoint
     next();
-    
+
   } catch (error) {
     response.status(401).json({
       error: new Error("Invalid request!"),
